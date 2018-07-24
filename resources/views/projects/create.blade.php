523f5806bdfbc6628fa1c3418191fe809855398d
Jordan Alamilla <Jordan@Jordans-MacBook-Air.local>
2018-07-24 17:29:35 -0400

    completed structure of create project form

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -26,6 +26,44 @@
     {!! Form::label('date', 'Date'); !!}
     {!! Form::text('date', '', ['placeholder' => 'Month Year']); !!}
 
+    <br>
+    
+    <!--PRINTS AVAILABLE-->
+    {!! Form::label('prints_available', 'Prints Available'); !!}
+    {!! Form::text('prints_available', '', ['placeholder' => '10']); !!}
+
+    <br>
+    
+    <!--PRINT SIZE-->
+    {!! Form::label('print_size', 'Print Size'); !!}
+    {!! Form::text('print_size', '', ['placeholder' => '11 x 15 inch']); !!}
+
+    <br>
+    
+    <!--PRINT PRICE-->
+    {!! Form::label('print_price', 'Print Price'); !!}
+    {!! Form::text('print_price', '', ['placeholder' => '50.00']); !!}
+
+    <br>
+
+    <!--ORIGINAL AVAILABLE-->
+    {!! Form::label('original_avaiable', 'Original Available?'); !!}
+    {!! Form::checkbox('original_avaiable', '', true); !!}
+
+    <div id="original-details">
+
+        <!--ORIGINAL SIZE-->
+        {!! Form::label('original_size', 'Original Size'); !!}
+        {!! Form::text('original_size', '', ['placeholder' => 'Month Year']); !!}
+
+        <br>
+
+        <!--ORIGINAL PRICE-->
+        {!! Form::label('original_price', 'Original Price'); !!}
+        {!! Form::text('original_price', '', ['placeholder' => '800.00']); !!}
+
+    </div>
+
 {!! Form::close() !!}
 
 @endsection
