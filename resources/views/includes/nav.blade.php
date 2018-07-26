59a834131d5ed986486266de753ab763936a555b
Jordan Alamilla <Jordan@Jordans-MacBook-Air.local>
2018-07-26 11:08:49 -0400

    made adjustments to authentication functionalities

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -30,43 +30,47 @@
 
                 {{-- LOGGED IN LINKS --}}
                 @else
-                    
-                    <li><a href="/dashboard">Dashboard</a></li>
+                        <li class="nav-item dropdown">
 
-                    <li class="nav-item dropdown">
+                            <a id="navbarDropdown"
+                            class="nav-link dropdown-toggle"
+                            href="#"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false" v-pre>
 
-                        <a id="navbarDropdown"
-                           class="nav-link dropdown-toggle"
-                           href="#"
-                           role="button"
-                           data-toggle="dropdown"
-                           aria-haspopup="true"
-                           aria-expanded="false" v-pre>
-
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right"
-                             aria-labelledby="navbarDropdown">
-
-                            <a class="dropdown-item"
-                               href="{{ route('logout') }}"
-
-                               onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-
-                                {{ __('Logout') }}
+                                {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
-                            <form id="logout-form"
-                                  action="{{ route('logout') }}"
-                                  method="POST"
-                                  style="display: none;">
-                                @csrf
-                            </form>
-                            
-                        </div>
-                    </li>
+                            <div class="dropdown-menu dropdown-menu-right"
+                                aria-labelledby="navbarDropdown">
+
+                                @if( Auth::user()->type == 'admin' )
+                                    <a href="/dashboard">Dashboard</a>
+                                @endif
+
+                                <a href="/cart">Cart</a>
+
+                                <a class="dropdown-item"
+                                href="{{ route('logout') }}"
+
+                                onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form"
+                                    action="{{ route('logout') }}"
+                                    method="POST"
+                                    style="display: none;">
+                                    @csrf
+                                </form>
+
+                            </div>
+                        </li>
+
                 @endguest
             </ul>
         </div>
