ccdcb9e8c9a9d2f34b800b82b698504956ffcc9d
Jordan Alamilla <jordan.alamilla@gmail.com>
2018-09-11 11:17:54 -0400

    added "print" and "original" sections to create form

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -69,6 +69,7 @@
                             type="text"
                             class="full space"
                             name="credits"
+                            placeholder="Modeling by Liv Sage"
                             value="{{ old('credits') }}">
     
                     </div>
@@ -86,6 +87,7 @@
                             type="text"
                             class="full space"
                             name="type"
+                            placeholder="Charcoal drawing"
                             value="{{ old('type') }}"
                             required>
     
@@ -123,6 +125,144 @@
                             name="full_image"
                             required>
     
+                    </div>
+    
+                </div>
+
+                <div class="half left space form-create-product">
+
+                    <div class="space">
+                        <h2>Print</h2>
+                    </div>
+
+                    <div id="form-print">
+
+                        <!-- AVAILABLE -->
+                        <div class="form-group padding">
+            
+                            <label class="space lowercase" for="prints-available">Available</label>
+            
+                            <div>
+
+                                <input id="prints-available"
+                                    type="number"
+                                    class="full space"
+                                    name="prints-available"
+                                    placeholder="10"
+                                    value="{{ old('prints-available') }}"
+                                    required>
+            
+                            </div>
+
+                        </div>
+
+                        <!-- SIZE -->
+                        <div class="form-group padding">
+            
+                            <label class="space lowercase" for="print-size">Size</label>
+            
+                            <div>
+
+                                <input id="print-size"
+                                    type="text"
+                                    class="full space"
+                                    name="print-size"
+                                    placeholder="11 x 15 Inches"
+                                    value="{{ old('print-size') }}"
+                                    required>
+            
+                            </div>
+
+                        </div>
+
+                        <!-- PRICE -->
+                        <div class="form-group padding">
+            
+                            <label class="space lowercase" for="print-price">Price</label>
+            
+                            <div>
+
+                                <input id="print-price"
+                                    type="text"
+                                    class="full space"
+                                    name="print-price"
+                                    placeholder="$50.00"
+                                    value="{{ old('print-price') }}"
+                                    required>
+            
+                            </div>
+
+                        </div>
+
+                    </div>
+    
+                </div>
+
+                <div class="half left space form-create-product">
+
+                    <div class="space">
+                        <h2>Original</h2>
+                    </div>
+
+                    <div id="form-print">
+
+                        <!-- AVAILABLE -->
+                        <div class="form-group padding">
+            
+                            <label class="space lowercase" for="original-available">Available</label>
+            
+                            <div>
+
+                                <input id="original-available"
+                                    type="number"
+                                    class="full space"
+                                    name="original-available"
+                                    placeholder="1"
+                                    value="{{ old('original-available') }}"
+                                    required>
+            
+                            </div>
+
+                        </div>
+
+                        <!-- SIZE -->
+                        <div class="form-group padding">
+            
+                            <label class="space lowercase" for="original-size">Size</label>
+            
+                            <div>
+
+                                <input id="original-size"
+                                    type="text"
+                                    class="full space"
+                                    name="original-size"
+                                    placeholder="18 x 24 Inches"
+                                    value="{{ old('original-size') }}"
+                                    required>
+            
+                            </div>
+
+                        </div>
+
+                        <!-- PRICE -->
+                        <div class="form-group padding">
+            
+                            <label class="space lowercase" for="original-price">Price</label>
+            
+                            <div>
+
+                                <input id="original-price"
+                                    type="text"
+                                    class="full space"
+                                    name="original-price"
+                                    placeholder="$800.00"
+                                    value="{{ old('original-price') }}"
+                                    required>
+            
+                            </div>
+
+                        </div>
+
                     </div>
     
                 </div>
